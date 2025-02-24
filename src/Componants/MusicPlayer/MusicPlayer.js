5d4b31fad9341226e5249ca4cbc104c9dd6ce0a1
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2025-02-25 00:39:22 +0530

    Update MusicPlayer.js

--- src/Componants/MusicPlayer/MusicPlayer.js
+++ src/Componants/MusicPlayer/MusicPlayer.js
@@ -16,7 +16,6 @@ const MusicPlayer = () => {
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0); // perticuler duration for song
   const audioRef = useRef(null); //to change Song
-
   /**
    * Play & Pasue toggle
    */
@@ -30,7 +29,6 @@ const MusicPlayer = () => {
       setIsPlaying(false);
     }
   };
-
   /**
    * onChange for Change songs
    */
@@ -49,20 +47,31 @@ const MusicPlayer = () => {
   };
 
   useEffect(() => {
+
     const audio = audioRef.current;
+
     const updateCurrentTime = () => setCurrentTime(audio.currentTime);
+
     const updateDuration = () => setDuration(audio.duration);
+
     audio.addEventListener("timeupdate", updateCurrentTime);
+
     audio.addEventListener("loadedmetadata", updateDuration);
 
     if (isPlaying) {
+
       audio.play();
+
     }
 
     return () => {
+
       audio.removeEventListener("timeupdate", updateCurrentTime);
+
       audio.removeEventListener("loadedmetadata", updateDuration);
+
     };
+
   }, [currentSong, isPlaying]);
 
   const songs = [
@@ -73,18 +82,21 @@ const MusicPlayer = () => {
       cover: "https://i.ibb.co/ZS3wRSh/cover.jpg",
       duration: "04:30",
     },
+
     {
       title: "Bad Liar",
       artist: "Imagine Dragons",
       cover: "https://samplesongs.netlify.app/album-arts/bad-liar.jpg",
       src: "https://samplesongs.netlify.app/Bad%20Liar.mp3",
     },
+
     {
       title: "Faded",
       artist: "Alan Walker",
       cover: "https://samplesongs.netlify.app/album-arts/faded.jpg",
       src: "https://samplesongs.netlify.app/Faded.mp3",
     },
+
     {
       title: "Hate Me",
       artist: "Ellie Goulding",
@@ -92,43 +104,63 @@ const MusicPlayer = () => {
       src: "https://samplesongs.netlify.app/Hate%20Me.mp3",
       id: "4",
     },
+
     {
       title: "Solo",
       artist: "Clean Bandit",
       cover: "https://samplesongs.netlify.app/album-arts/solo.jpg",
       src: "https://samplesongs.netlify.app/Solo.mp3",
     },
+
     {
       title: "Without Me",
       artist: "Halsey",
       cover: "https://samplesongs.netlify.app/album-arts/without-me.jpg",
       src: "https://samplesongs.netlify.app/Without%20Me.mp3",
     },
+
   ];
 
   return (
     <>
+
       <div className={`container song-${currentSong}`}>
+
         <div className="player">
+
           <div className="player__controls">
+
             <div className="player__btn player__btn--small" id="previous">
+
               <FontAwesomeIcon icon={faArrowLeft} />
+
             </div>
+
             <h5 className="player__title">playing now</h5>
+
             <div className="player__btn player__btn--small" id="icon-menu">
+
               <FontAwesomeIcon icon={faBars} />
+
             </div>
+
           </div>
+
           <div className="player__album">
+
             <img
               src={songs[currentSong].cover}
               alt="Album Cover"
               className="player__img"
               loading="lazy"
             />
+
           </div>
+
           <h2 className="player__artist">{songs[currentSong].artist}</h2>
+
           <h3 className="player__song">{songs[currentSong].title}</h3>
+
           <input
             type="range"
             value={currentTime}
@@ -137,14 +169,23 @@ const MusicPlayer = () => {
             id="range"
             onChange={(e) => (audioRef.current.currentTime = e.target.value)}
           />
+
           <div className="audio-duration">
+
             <div className="start">{formatTime(currentTime)}</div>
+
             <div className="end">{formatTime(duration)}</div>
+
           </div>
+
           <audio className="player__audio" controls id="audio" ref={audioRef}>
+
             <source src={songs[currentSong].src} type="audio/mpeg" />
+
           </audio>
+
           <div className="player__controls">
+
             <div
               className="player__btn player__btn--medium"
               id="backward"
@@ -152,35 +193,51 @@ const MusicPlayer = () => {
                 changeSong((currentSong - 1 + songs.length) % songs.length)
               }
             >
+
               <FontAwesomeIcon icon={faBackward} />
+
             </div>
+
             <div
               className="player__btn player__btn--medium blue play"
               id="play"
               onClick={togglePlayPause}
             >
+
               <FontAwesomeIcon
                 icon={faPlay}
                 className={`play-btn ${isPlaying ? "hide" : ""}`}
               />
+
               <FontAwesomeIcon
                 icon={faPause}
                 className={`pause-btn ${isPlaying ? "" : "hide"}`}
               />
+
             </div>
+
             <div
               className="player__btn player__btn--medium"
               id="forward"
               onClick={() => changeSong((currentSong + 1) % songs.length)}
             >
+
               <FontAwesomeIcon icon={faForward} />
+
             </div>
+
           </div>
+
         </div>
+
         <div className="songs-list">
+
           <div className="player__song-list">
+
             <h4 className="player__song-list-title">Songs</h4>
+                
             <ul className="player__song-list-items">
+
               {songs.map((song, index) => (
                 <li
                   key={index}
@@ -192,10 +249,15 @@ const MusicPlayer = () => {
                   {song.title} - {song.artist}
                 </li>
               ))}
+
             </ul>
+
           </div>
+
         </div>
+
       </div>
+
     </>
   );
 };
