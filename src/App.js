0ada4c5447dad7cda7c895ea88328315c0e4b343
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2025-09-18 20:38:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,1 @@
-import logo from './logo.svg';
-import './App.css';
-import MusicPlayer from './Componants/MusicPlayer/MusicPlayer';
-
-function App() {
-  return (
-    <div className="App">
-      <MusicPlayer/>
-    </div>
-  );
-}
-
-export default App;
-
 
